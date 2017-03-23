60d334fe6fa86ec248644a9259960135cff7ca23
ItGeneral <799178171@qq.com>
2017-03-23 17:13:16 +0800

    add login relation

--- src/main/java/com/web/user/controller/LoginController.java
+++ src/main/java/com/web/user/controller/LoginController.java
@@ -1,17 +1,21 @@
 package com.web.user.controller;
 
-import com.web.intercepter.LoginInterceptor;
+import com.web.base.BaseController;
 import com.web.user.model.User;
 import com.web.user.service.UserService;
 import com.web.utils.MD5Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,45 +26,49 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Controller
 @RequestMapping(value = "/login")
-public class LoginController {
+public class LoginController extends BaseController{
 
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     private UserService userService;
     /**
-     * 登录
-     * 1、
+     * 登陆后 用userId和privateKey（秘钥）生成一个签名，并将userId和签名一同返回给前端
      * @param request
      * @param response
      */
     @RequestMapping(value = "")
-    public String login(HttpServletRequest request, HttpServletResponse response, @RequestParam String userName, @RequestParam String password){
+    @ResponseBody
+    public ResponseEntity login(HttpServletRequest request, HttpServletResponse response, @RequestParam String userName, @RequestParam String password){
         try{
-            User user = userService.queryUserByUserName(userName);
-            //用户名不存在
+            //User user = userService.queryUserByUserName(userName);
+            User user = new User();
+            user.setId(121L);
+            user.setPassword("01D7F40760960E7BD9443513F22AB9AF");
             if (user == null){
-
+                return badRequest(HttpStatus.BAD_REQUEST).put("message", "用户名不存在").build();
             }
-            String mdPassword = MD5Util.getMD5String(password);
-            //密码不正确
+            String mdPassword = MD5Util.getEncryptedString(password);
             if (!user.getPassword().equals(mdPassword)){
-
+                return badRequest(HttpStatus.BAD_REQUEST).put("message", "密码不正确").build();
             }
-            request.getSession().setAttribute("login_user", user);
-
-            //将登陆信息返回
+            //将userId和signal（签名）返回前端cookie中
+            request.setAttribute("userId", user.getId().toString());
+            request.setAttribute("signal", MD5Util.getEncryptedString(user.getId().toString(), "SHA-256"));
 
         }catch (Exception e){
             logger.error("登录异常", e);
         }
-        //登录成功跳转至首页
-        return "index";
+        return ok().put("message", "登录成功").build();
     }
 
     @RequestMapping(value = "register")
     public void register(@RequestBody User user){
 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MD5Util.getEncryptedString("zhangsan"));
     }
 
 }
--- src/main/java/com/web/utils/MD5Util.java
+++ src/main/java/com/web/utils/MD5Util.java
@@ -13,24 +13,48 @@ import java.security.MessageDigest;
 public class MD5Util {
 
     private static final Logger logger = LoggerFactory.getLogger(MD5Util.class);
+
+    private final static String privateKey = "";
+
     /**
-     * 生成MD5
+     * 加密算法
+     * @param target
+     * @param algorithmType 加密类型
+     * @return
+     */
+    public static String getEncryptedString(String target, String algorithmType){
+        return getEncryptedString(target, algorithmType, privateKey);
+    }
+
+    /**
+     * 加密算法
      * @param target
      * @return
-     * @throws Exception
      */
-    public static String getMD5String(String target){
+    public static String getEncryptedString(String target){
+        return getEncryptedString(target, "MD5", "");
+    }
+
+    /**
+     * 加密
+     * @param target
+     * @param algorithmType 加密类型 MD5, SHA-1, SHA-256.
+     * @param privateKey 秘钥
+     * @return
+     */
+    public static String getEncryptedString(String target, String algorithmType, String privateKey){
+        target = target + privateKey;
         String md5String = "";
         try {
             //1 创建一个提供信息摘要算法的对象，初始化为md5算法对象
-            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            MessageDigest messageDigest = MessageDigest.getInstance(algorithmType);
             //2 将消息变成byte数组
             byte[] targetBytes = target.getBytes();
             //3 计算后获得字节数组,这就是那128位了
             byte[] buff = messageDigest.digest(targetBytes);
             md5String = bytesToHex(buff);
         } catch (Exception e) {
-            logger.error("md5密码加密错误", e);
+            logger.error("加密异常", e);
         }
         return md5String;
     }
@@ -55,8 +79,6 @@ public class MD5Util {
         }
         return md5Str.toString().toUpperCase();
     }
-
-
 
 
 }
